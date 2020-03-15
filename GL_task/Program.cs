6074d0c9c70abbf277a0a476aca02b73cb8f686e
pavelkovasyl <pavelkovasyl@gmail.com>
2020-03-15 15:38:55 +0200

    Beta version

--- GL_task/Program.cs
+++ GL_task/Program.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.IO;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.Unicode;
 //using Newtonsoft.Json;
 
 
@@ -15,76 +17,112 @@ namespace GL_task
     {
         
 
-        class CatalogInfo
+        class Catalog
         {
+            public static JsonSerializerOptions serializeOptions = new JsonSerializerOptions();
+
+
+            //return Folder with files and nested folders
             public static Folder GetFolder(string path)
             {
-                //create result Folder
-                var resultFolder = new Folder();
-                resultFolder.Files = new List<File>();
-                resultFolder.Children = new List<Folder>();
-
-
-
                 DirectoryInfo dirInfo = new DirectoryInfo(path);
 
-                //Name
-                resultFolder.Name = dirInfo.Name;
-
-                //DateCreated
-                resultFolder.DateCreated = dirInfo.CreationTime;
-
-                //Get a reference to each file in that directory.
-                FileInfo[] fiArr = dirInfo.GetFiles();
-
-                if (fiArr.Length >0)
+                if (dirInfo.Exists)
                 {
-                    //ADD FILES
-                    foreach (FileInfo f in fiArr)
+                    //create result Folder
+                    var resultFolder = new Folder();
+                    resultFolder.Files = new List<File>();
+                    resultFolder.Children = new List<Folder>();
+
+                    //Name
+                    resultFolder.Name = dirInfo.Name;
+
+                    //DateCreated
+                    resultFolder.DateCreated = dirInfo.CreationTime;
+
+                    //Get a reference to each file in that directory.
+                    FileInfo[] fiArr = dirInfo.GetFiles();
+
+                    if (fiArr.Length > 0)
                     {
-                        var file = new File();
+                        //ADD FILES
+                        foreach (FileInfo f in fiArr)
+                        {
+                            var file = new File();
 
-                        //Name
-                        file.Name = f.Name;
+                            //Name
+                            file.Name = f.Name;
 
-                        //Size
-                        file.Size = String.Format($"{f.Length} B");
+                            //Size
+                            file.Size = String.Format($"{f.Length} B");
 
-                        //Path  f.TOstring or f.DirectoryName
-                        file.Path = f.FullName;
+                            //Path  f.TOstring or f.DirectoryName
+                            file.Path = f.FullName;
 
-                        //Add file to Files in Folder class 
-                        resultFolder.Files.Add(file);
-
+                            //Add file to Files in Folder class 
+                            resultFolder.Files.Add(file);
+                        }
                     }
+                    else
+                        resultFolder.Files = null;
+
+
+                    //WORKING WITH CHILDREN/SUB FOLDERS     //dirs - directories
+
+                    string[] dirs = Directory.GetDirectories(path);
+
+                    if (dirs.Length > 0)
+                    {
+                        foreach (var item in dirs)
+                        {
+                            Folder f = new Folder();
+                            f = GetFolder(item);
+                            resultFolder.Children.Add(f);
+                        }
+                    }
+                    else
+                        resultFolder.Children = null;
+
+                    //return Folder
+                    return resultFolder;
                 }
                 else
-                    resultFolder.Files = null;
-
-
-                //WORKING WITH CHILDREN/SUB FOLDERS
-
-                string[] dirs = Directory.GetDirectories(path);
-
-                if (dirs.Length >0)
                 {
-                    foreach (var item in dirs)
-                    {
-                        Folder f = new Folder();
-                        f = GetFolder(item);
-                        resultFolder.Children.Add(f);
-                    }
+                    Console.WriteLine("There is no folder with this name/path");
+                    //no folder result
+                    return null;
                 }
-                else
-                    resultFolder.Children = null;
+                
+            }
 
+            //Serialize Folder to Json file "Result.json"
+            public static void SerializedFolderToJsonFile(Folder folder)
+            {
+                serializeOptions.Converters.Add(new DateTimeConverter());
+                serializeOptions.WriteIndented = true;
+                serializeOptions.IgnoreNullValues = true;
+                serializeOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic);
 
+                // Saving to json file
+                using (FileStream fs = new FileStream("Result.json", FileMode.OpenOrCreate))
+                {
+                    JsonSerializer.SerializeAsync<Folder>(fs, folder, serializeOptions);
+                    Console.WriteLine("Data has been saved to file");
+                }
 
+                
+            }
 
-                //return Folder
-                return resultFolder;
+            //Showing serialized Folder in Console
+            public static void ShowSerializedFolder(Folder folder)
+            {
+                
+                string json = JsonSerializer.Serialize<Folder>(folder, serializeOptions);
+                Console.WriteLine(json);
             }
         }
+
+
         class Folder
         {
             public string Name { get; set; }
@@ -92,6 +130,8 @@ namespace GL_task
             public DateTime DateCreated { get; set; }
 
             public List <File> Files{ get; set; }
+            
+            //nested Folders
             public List <Folder> Children { get; set; }
 
         }
@@ -108,63 +148,26 @@ namespace GL_task
 
     static void Main(string[] args)
         {
-            string path = @"C:\TestGL";
+
+            //INPUT PATH
+            string path = @"C:\Програми";
 
            
+            Folder folder = Catalog.GetFolder(path);
 
-            Folder folder = CatalogInfo.GetFolder(path);
+            //Serialize Folder to Json file "Result.json"
+            Catalog.SerializedFolderToJsonFile(folder);
 
-
-            var serializeOptions = new JsonSerializerOptions();
-            serializeOptions.Converters.Add(new DateTimeConverter());
-            serializeOptions.WriteIndented = true;
-            serializeOptions.IgnoreNullValues = true;
-            string json = JsonSerializer.Serialize<Folder>(folder,serializeOptions);
-            Console.WriteLine(json);
+            //Show result in Console
+            Catalog.ShowSerializedFolder(folder);
 
 
-            //// сохранение данных
-            //using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
-            //{
-            //    Folder folder = new Folder() { Name = "Project", DateCreated = DateTime.Now };
-            //    JsonSerializer.SerializeAsync<Folder>(fs, folder);
-            //    Console.WriteLine("Data has been saved to file");
-            //}
+            
 
+            
 
-
-            //DirectoryInfo dirInfo = new DirectoryInfo(path);
-            //Console.WriteLine($"Название каталога: {dirInfo.Name}");
-            //Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
-
-
-
-            //// Make a reference to a directory.
-            //DirectoryInfo di = new DirectoryInfo("c:\\");
-            //// Get a reference to each file in that directory.
-            //FileInfo[] fiArr = di.GetFiles();
-            //// Display the names and sizes of the files.
-            //Console.WriteLine("The directory {0} contains the following files:", di.Name);
-            //foreach (FileInfo f in fiArr)
-            //    Console.WriteLine("The size of {0} is {1} bytes.", f.Name, f.Length);
-
-            //if (Directory.Exists(path))
-            //{
-            //    Console.WriteLine("Подкаталоги:");
-            //    string[] dirs = Directory.GetDirectories(path);
-            //    foreach (string s in dirs)
-            //    {
-            //        Console.WriteLine(s);
-            //    }
-            //    Console.WriteLine();
-            //    Console.WriteLine("Файлы:");
-            //    string[] files = Directory.GetFiles(path);
-            //    foreach (string s in files)
-            //    {
-            //        Console.WriteLine(s);
-            //        Console.WriteLine(s.Length);
-            //    }
+           
         }
-        
+
     }
 }
